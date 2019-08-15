7a6dd78895ed6c83940748e9e1d067b4b5947195
Alexandrina <lizurchik.a@gmail.com>
2019-08-15 23:38:49 +0300

    feat: update files

---
+++ precache-manifest.51f7d1845e3e39c9b8edd8e72ec8d316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913496e310713768866361054ca278b5",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "9bdcac06d122220660e4",
+    "url": "/chat/static/css/main.5cc11c7a.chunk.css"
+  },
+  {
+    "revision": "af21d3d65c381dca5a0e",
+    "url": "/chat/static/js/2.d9036806.chunk.js"
+  },
+  {
+    "revision": "9bdcac06d122220660e4",
+    "url": "/chat/static/js/main.f90659bc.chunk.js"
+  },
+  {
+    "revision": "2878249f4c4ae1237cb7",
+    "url": "/chat/static/js/runtime~main.eff7a0c2.js"
+  },
+  {
+    "revision": "baf40baa6b641f67f91a79254d765ee5",
+    "url": "/chat/static/media/unicorn.baf40baa.png"
+  }
+]);
